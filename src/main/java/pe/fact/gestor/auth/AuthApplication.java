90004c6d776235d02cdcee4d22fec76fd331bb20
watercannon <0333241024@unjfsc.edu.pe>
2026-01-14 18:58:49 -0500

    testeo para el war y este todo ok

--- src/main/java/pe/fact/gestor/auth/AuthApplication.java
+++ src/main/java/pe/fact/gestor/auth/AuthApplication.java
@@ -2,14 +2,20 @@ package pe.fact.gestor.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan; // Importar
 
 @SpringBootApplication
-@ComponentScan(basePackages = "pe.fact.gestor")
-public class AuthApplication {
+@ComponentScan(basePackages = {"pe.fact.gestor"})
+public class AuthApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AuthApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
-
 }
